85ae655e83b95f12f72de52ee6d0423c95656a05
Simon Mukonda <simonmktonto@gmail.com>
2026-09-05 18:50:06 +0200

    Fix swagger host for production

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'APIDev Catalog API',
         description: 'CRUD API for cataloging developer APIs - CSE 341 Project 2'
     },
-    host: process.env.NODE_ENV === 'production' ? 'project2-YOUR-RENDER-URL.onrender.com' : 'localhost:3002',
+    host: process.env.NODE_ENV === 'production' ? 'cse341-project2-nw1s.onrender.com' : 'localhost:3002',
     schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http']
 };
 
